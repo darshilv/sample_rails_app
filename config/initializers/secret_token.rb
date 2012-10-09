709c2e8fd2f039c9b45c1bba254518fa8d38e6e8
Darshil Vora <darshilv@gmail.com>
2012-10-08 22:51:55 -0700

    initial push with Twitter Bootstrap and sample User Object

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleRailsApp::Application.config.secret_token = '3c66ff89615e66bdfafea0dcf75b64890290bf7d075a7478d1c1bd6522910ef7d72bbc360d83a6447549c653bfcf8f239f888967a930217d261ce1a9fd06bc33'
